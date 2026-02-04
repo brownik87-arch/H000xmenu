8780aa9549d969afd12be1c7eb4e221020ff4ce3
brownik87-arch <brownik87@gmail.com>
2026-02-04 17:22:21 +0500

    Update H00xmenu.lua

--- H00xmenu.lua
+++ H00xmenu.lua
@@ -1,5 +1,2 @@
-local Library = loadstring(game:HttpGet("https://github.com/brownik87-arch/H000xmenu/blob/main/H00xmenu.lua"))()
 
-local Window = Library.CreateLib("H00xmenu", "RJTheme3")
-
-local Section = Tab:NewSection("tools")
+Library.CreateLib("H00xmenu", "RJTheme3")
