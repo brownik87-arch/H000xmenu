720ed449d4e1548639b8f7c512af9898379d73a5
brownik87-arch <brownik87@gmail.com>
2026-02-04 17:45:47 +0500

    Update H00xmenu.lua

--- H00xmenu.lua
+++ H00xmenu.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("H00xmenu From H00xteam", "Red")
+local Window = Library.CreateLib("H00xmenu From H00xteam", "RJTheme3")
 
 local Section = Tab:NewSection("Tools")
 
