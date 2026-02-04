e06a69c1cbebae99ba8bd5f1b6e2d194ebec72b3
brownik87-arch <brownik87@gmail.com>
2026-02-04 18:59:47 +0500

    Update H00xmenu.lua

--- H00xmenu.lua
+++ H00xmenu.lua
@@ -8,52 +8,76 @@ local Section = Tab:NewSection("Speed")
 
 Section:NewToggle("ToggleText", "ToggleInfo", function(state)
     if state then
+        script.Disabled = false
+        
         Local = game:GetService('Players').LocalPlayer
-Char  = Local.Character
-touched,tpdback = false, false
-Local.CharacterAdded:connect(function(char)
-    if script.Disabled ~= true then
-        wait(.25)
-        loc = Char.HumanoidRootPart.Position
-        Char:MoveTo(box.Position + Vector3.new(0,.5,0))
-    end
-end)
-game:GetService('UserInputService').InputBegan:connect(function(key)
-    if key.KeyCode == Enum.KeyCode.Equals then
-        if script.Disabled ~= true then
-            script.Disabled = true
-            print'you may re-execute'
-        end
-    end
-end)
-box = Instance.new('Part',workspace)
-box.Anchored = true
-box.CanCollide = true
-box.Size = Vector3.new(10,1,10)
-box.Position = Vector3.new(0,10000,0)
-box.Touched:connect(function(part)
-    if (part.Parent.Name == Local.Name) then
-        if touched == false then
-            touched = true
-            function apply()
-                if script.Disabled ~= true then
-                    no = Char.HumanoidRootPart:Clone()
-                    wait(.25)
-                    Char.HumanoidRootPart:Destroy()
-                    no.Parent = Char
-                    Char:MoveTo(loc)
-                    touched = false
-                end end
-            if Char then
-                apply()
+        Char = Local.Character
+        touched, tpdback = false, false
+        
+        local charAddedConn = Local.CharacterAdded:Connect(function(char)
+            if script.Disabled ~= true then
+                wait(0.25)
+                loc = Char.HumanoidRootPart.Position
+                Char:MoveTo(box.Position + Vector3.new(0, 0.5, 0))
             end
-        end
-    end
-end)
-repeat wait() until Char
-loc = Char.HumanoidRootPart.Position
-Char:MoveTo(box.Position + Vector3.new(0,.5,0))
+        end)
+        
+        local inputConn = game:GetService('UserInputService').InputBegan:Connect(function(key)
+            if key.KeyCode == Enum.KeyCode.Equals then
+                if script.Disabled ~= true then
+                    script.Disabled = true
+                    print("you may re-execute")
+                end
+            end
+        end)
+        
+        box = Instance.new('Part', workspace)
+        box.Anchored = true
+        box.CanCollide = true
+        box.Size = Vector3.new(10, 1, 10)
+        box.Position = Vector3.new(0, 10000, 0)
+        
+        local touchedConn = box.Touched:Connect(function(part)
+            if (part.Parent.Name == Local.Name) then
+                if touched == false then
+                    touched = true
+                    function apply()
+                        if script.Disabled ~= true then
+                            no = Char.HumanoidRootPart:Clone()
+                            wait(0.25)
+                            Char.HumanoidRootPart:Destroy()
+                            no.Parent = Char
+                            Char:MoveTo(loc)
+                            touched = false
+                        end
+                    end
+                    if Char then
+                        apply()
+                    end
+                end
+            end
+        end)
+        
+        repeat wait() until Char
+        loc = Char.HumanoidRootPart.Position
+        Char:MoveTo(box.Position + Vector3.new(0, 0.5, 0))
+        
+        connections = {charAddedConn, inputConn, touchedConn}
+        
     else
+        script.Disabled = true
         print("Toggle Off")
+        
+        -- Удаляем платформу
+        if box and box.Parent then
+            box:Destroy()
+        end
+        
+        if connections then
+            for _, conn in ipairs(connections) do
+                conn:Disconnect()
+            end
+            connections = nil
+        end
     end
 end)
