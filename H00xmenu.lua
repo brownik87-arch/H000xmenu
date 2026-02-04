3e536043da30cf5b801c15e277c2d8f971198c82
brownik87-arch <brownik87@gmail.com>
2026-02-04 18:20:21 +0500

    Update H00xmenu.lua

--- H00xmenu.lua
+++ H00xmenu.lua
@@ -4,4 +4,8 @@ local Window = Library.CreateLib("H00xmenu From H00xteam", "RJTheme3")
 
 local Tab = Window:NewTab("Tools speed")
 
-local Section = Tab:NewSection("names")
+local Section = Tab:NewSection("Speed")
+
+Section:NewSlider("Speed", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
