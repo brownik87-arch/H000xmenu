e0a92e22f0d33a4dad4266ed459f6befbb4162ed
brownik87-arch <brownik87@gmail.com>
2026-02-22 19:13:13 +0500

    Update H00xmenu.lua

--- H00xmenu.lua
+++ H00xmenu.lua
@@ -33,3 +33,29 @@ Name.TextScaled = true
 Name.TextSize = 14.000 
 Name.TextWrapped = true 
 
+e1x1x1x1Message.Name = "e1x1x1x1Message" 
+e1x1x1x1Message.Parent = Frame 
+e1x1x1x1Message.BackgroundColor3 = Color3.fromRGB(255, 0, 0) 
+e1x1x1x1Message.Position = UDim2.new(0.0714285746, 0, 0.216000006, 0) 
+e1x1x1x1Message.Size = UDim2.new(0, 79, 0, 31) 
+e1x1x1x1Message.Font = Enum.Font.GothamBold 
+e1x1x1x1Message.Text = "Help Me Build A Boat Creepypasta" 
+e1x1x1x1Message.TextColor3 = Color3.fromRGB(0, 0, 0) 
+e1x1x1x1Message.TextScaled = true 
+e1x1x1x1Message.TextSize = 14.000 
+e1x1x1x1Message.TextWrapped = true 
+e1x1x1x1Message.MouseButton1Down:connect(function() 
+ while true do 
+ msg = Instance.new ("Message") 
+ msg.Parent = game.Workspace 
+ msg.Text = "BABFT Privace Experience Creepypasta Is Here." 
+ game.Lighting.TimeOfDay = "18:00:00" 
+ wait(6) 
+ msg:remove() 
+ wait(3) 
+ msg = Instance.new("Message") 
+ msg.Parent = game.Workspace 
+ msg.Text = "" 
+ 
+ end 
+end) 
